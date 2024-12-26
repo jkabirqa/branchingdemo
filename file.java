484d2a91ee8e43b7cb83abe6a92cd3316982e2d3
abul <a@abc.com>
2024-12-25 20:36:35 -0500

    java file is modified

---
+++ file.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get user input
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Choose an operation (+, -, *, /): ");
+        char operation = scanner.next().charAt(0);
+
+        // Perform the operation and display the result
+        switch (operation) {
+            case '+':
+                System.out.println("Result: " + (num1 + num2));
+                break;
+            case '-':
+                System.out.println("Result: " + (num1 - num2));
+                break;
+            case '*':
+                System.out.println("Result: " + (num1 * num2));
+                break;
+            case '/':
+                if (num2 != 0) {
+                    System.out.println("Result: " + (num1 / num2));
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Invalid operation.");
+        }
+
+        scanner.close();
+    }
+}
+
